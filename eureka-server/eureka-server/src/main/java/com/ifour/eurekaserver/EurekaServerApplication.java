de2fd1bdcdc145fcbb28bf9028d4b38dc47e8f6b
yashshukla1319 <81147963+yashshukla1319@users.noreply.github.com>
2021-07-28 17:35:21 +0530

    new

--- eureka-server/eureka-server/src/main/java/com/ifour/eurekaserver/EurekaServerApplication.java
+++ eureka-server/eureka-server/src/main/java/com/ifour/eurekaserver/EurekaServerApplication.java
@@ -4,13 +4,35 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableEurekaServer
 @SpringBootApplication
+@EnableSwagger2
 public class EurekaServerApplication {
 	@LoadBalanced
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+	@Bean
+	public Docket swaggerConfiguration()
+	{
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.build()
+				.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo()
+	{
+		return new ApiInfoBuilder().title("Eureka Server Documentation").version("1.0.0").build();
 	}
 
 }
